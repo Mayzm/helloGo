39493dfe5d3e405e01b814894afa81db35f3b5d4
zengmei <zengmei@inboyu.com>
2020-05-21 16:13:48 +0800

    test

--- common/helper.go
+++ common/helper.go
@@ -7,9 +7,15 @@ import (
 )
 
 func main() {
+	a, b := test(2)
+	fmt.Printf("a = %d, b = %d\n", a, b)
 	var goos string = runtime.GOOS
-	a := 1
 	fmt.Printf("The operating system is: %s\n", goos)
 	path := os.Getenv("PATH")
 	fmt.Printf("Path is %s\n", path)
+}
+
+func test(a) (int, int) {
+	key, value := 1, a
+	return key, value
 }
